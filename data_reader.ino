19f39524bf907d1a33f761afb5052be86bc5ed39
aurobots <emre.hmkk@gmail.com>
2017-04-13 12:10:57 +0300

    Add files via upload

---
+++ data_reader.ino
@@ -0,0 +1,15 @@
+//Serial printer for the EMG sensor
+//This code is for printing the EMG value on the
+//serial monitor.
+//
+//© Au Robots 8.4.2017
+
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+Serial.println(analogRead(x));
+}
+
